90a1ffaeae641b8780ad47a661faa35b1e77ec70
jrclegg <clegg.jonathan@rocketmail.com>
2017-07-24 18:48:50 +0100

    Wallaby Section, css and slide toggle added

--- rediscover.js
+++ rediscover.js
@@ -21,9 +21,17 @@ $(document).ready(function(){
 
 // Bavarian Pine Vole Slide Toggle //
 
-
 $(document).ready(function(){
     $(".vole-section").click(function(){
         $(".vole-summary").slideToggle("slow");
     });
 });
+
+// Wallaby Section Slide Toggle
+
+$(document).ready(function(){
+    $(".wallaby-section").click(function(){
+        $(".wallaby-summary").slideToggle("slow");
+    });
+});
+
